6339136885699d3d1b9d18390a2fb327be3b3013
jingyi <1849612320@qq.com>
2016-12-30 09:23:40 +0800

    Initial Commit

---
+++ fenye/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  fenye
+//
+//  Created by 691 on 2016/12/30.
+//  Copyright © 2016年 691. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
