e204d80d5c5b2d97d886f9b3743b05fca41d723a
jingyi <1849612320@qq.com>
2017-04-06 21:21:35 +0800

    first commit

---
+++ fenye/PageView.h
@@ -0,0 +1,13 @@
+//
+//  PageView.h
+//  fenye
+//
+//  Created by 691 on 2017/2/23.
+//  Copyright © 2017年 691. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PageView : UIView
+
+@end
---
+++ fenye/PageView.m
@@ -0,0 +1,21 @@
+//
+//  PageView.m
+//  fenye
+//
+//  Created by 691 on 2017/2/23.
+//  Copyright © 2017年 691. All rights reserved.
+//
+
+#import "PageView.h"
+
+@implementation PageView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
